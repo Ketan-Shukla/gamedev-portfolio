d8546877ae0545fea927289d7dfc18ebe545fa7e
Ketan Shukla <ketan.shukla@a5labs.co>
2024-07-08 10:41:06 +0530

    vercel build fix for canvas

--- next.config.js
+++ next.config.js
@@ -32,5 +32,14 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'source.unsplash.com'],
   },
 };
-
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 module.exports = nextConfig;
